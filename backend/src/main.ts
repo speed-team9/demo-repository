ae5b7f224d04895e8c683ce84bf6d0f01e791cbc
TimeToPretend <2553341751@qq.com>
2025-11-09 21:21:36 +0800

    deploy: backend as vercel serverless function

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,10 +5,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: "http://localhost:3000",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: ["http://localhost:3000", /^https:\/\/.*\.vercel\.app$/],
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE"],
     credentials: true,
   });
+
   const port = process.env.PORT || 8082;
   await app.listen(port, () => console.log(`Server running on port ${port}`));
 }
