14d5df6ed63046ed19eba9c0ec99da4553552b7d
TimeToPretend <2553341751@qq.com>
2025-11-12 23:31:41 +0800

    test: Unit Test

--- backend/eslint.config.mjs
+++ backend/eslint.config.mjs
@@ -6,7 +6,14 @@ import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   {
-    ignores: ['eslint.config.mjs'],
+    ignores: [
+      'eslint.config.mjs',
+      'jest.config.js',
+      'src/test/**',
+      'dist/**', 
+      'coverage/**',
+      'node_modules/**'
+    ],
   },
   eslint.configs.recommended,
   ...tseslint.configs.recommendedTypeChecked,
